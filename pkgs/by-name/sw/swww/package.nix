c536fb53c3df103647d1207b7243957338140282
LordMZTE <lord@mzte.de>
2025-05-27 15:39:04 +0200

    swww: 0.9.5 -> 0.10.0

--- pkgs/by-name/sw/swww/package.nix
+++ pkgs/by-name/sw/swww/package.nix
@@ -7,25 +7,30 @@
   libxkbcommon,
   installShellFiles,
   scdoc,
+  wayland,
+  wayland-protocols,
+  wayland-scanner,
 }:
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "swww";
-  version = "0.9.5";
+  version = "0.10.0";
 
   src = fetchFromGitHub {
     owner = "LGFae";
     repo = "swww";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-ldy9HhIsWdtTdvtRLV3qDT80oX646BI4Q+YX5wJXbsc=";
+    hash = "sha256-g7h8Ec1et4fWDTv34asFXea6SBIXv3D90Pd+YsnhbXQ=";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-K1ww0bOD747EDtqYkA0Dlu7cwbjYcPwSXPSqQDbTwZo=";
+  cargoHash = "sha256-cVCE6Mw0h/kXuuxsf8r55Uj9GAVtrxXof6sq3dfd0rk=";
 
   buildInputs = [
     lz4
     libxkbcommon
+    wayland
+    wayland-protocols
   ];
 
   doCheck = false; # Integration tests do not work in sandbox environment
@@ -35,6 +40,17 @@ rustPlatform.buildRustPackage (finalAttrs: {
     installShellFiles
     scdoc
   ];
+
+  postPatch = ''
+    # swww uses waybackend, an in-house wayland-scanner implementation. The build script tells it to
+    # locate `wayland.xml` via `WaylandProtocol::Client`. This is implemented by querying pkg-config
+    # for the data directory of the `wayland` package, which does not contain `wayland.xml` in
+    # nixpkgs since we package this file as a part of `wayland-scanner`. To fix this, we modify the
+    # upstream build script in order to give it the correct path.
+    substituteInPlace "daemon/build.rs" \
+      --replace-fail 'WaylandProtocol::Client' \
+        'WaylandProtocol::Local(PathBuf::from("${wayland-scanner}/share/wayland/wayland.xml"))'
+  '';
 
   postInstall = ''
     for f in doc/*.scd; do
